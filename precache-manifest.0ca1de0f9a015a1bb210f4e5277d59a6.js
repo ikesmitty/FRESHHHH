fb137759392be6172b5fa3654f20ba70c397d146
ikesmitty <smithi@mail.gvsu.edu>
2019-08-07 10:13:41 -0400

    Updates

---
+++ precache-manifest.0ca1de0f9a015a1bb210f4e5277d59a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d936223872826951459247e69ce261",
+    "url": "/FRESHHHH/index.html"
+  },
+  {
+    "revision": "8e3c80cde19305f84d4d",
+    "url": "/FRESHHHH/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/FRESHHHH/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8e3c80cde19305f84d4d",
+    "url": "/FRESHHHH/static/js/main.2f0b7778.chunk.js"
+  },
+  {
+    "revision": "8acd22ec57c859b81c6d",
+    "url": "/FRESHHHH/static/js/runtime~main.185f4861.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/FRESHHHH/static/media/logo.5d5d9eef.svg"
+  }
+]);
